26c9b066c3c682d5a35983629b3c1a4499d66c4a
nhipp <nhipp@DESKTOP-A1QJFB8>
2016-12-03 18:52:11 -0600

    Initial project setup

---
+++ VNKFinalProject/src/main/java/edu/unomaha/comm/net/vnk/semester/project/App.java
@@ -0,0 +1,14 @@
+package edu.unomaha.comm.net.vnk.semester.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
