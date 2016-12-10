7dd79b776ba4607eda7e6cead96ef258f1b7bece
nhipp <nhipp@DESKTOP-A1QJFB8>
2016-12-10 15:50:42 -0600

    Added more to sample

--- VNKFinalProject/src/main/java/edu/unomaha/comm/net/vnk/semester/project/ExampleSimple.java
+++ VNKFinalProject/src/main/java/edu/unomaha/comm/net/vnk/semester/project/ExampleSimple.java
@@ -1,6 +1,7 @@
 package edu.unomaha.comm.net.vnk.semester.project;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureDHT;
@@ -23,15 +24,28 @@ public class ExampleSimple {
 	}
 
 	public static void main(String[] args) throws NumberFormatException, Exception {
-//		args = new String[] { "1", "test.me", "192.168.1.1" };
-		args = new String[] { "2", "test.me" };
+//		args = new String[] { "1", "test.me", "Hello from peer 1" };
+//		args = new String[] { "2", "test.me" };
+//		args = new String[] { "3", "test.me" };
+//		args = new String[] { "3", "test.me.2", "Hello from peer 3" };
+//		args = new String[] { "4", "test.me.2" };
+//		args = new String[] { "5", "test.me.2" };
+//		args = new String[] { "6", "test.me" };
+		args = new String[] { "7", "test.me.2" };
+		System.out.println("Peer ID: " + args[0]);
 		ExampleSimple dns = new ExampleSimple(Integer.parseInt(args[0]));
 		if (args.length == 3) {
-			dns.store(args[1]);
+			dns.store(args[1], args[2]);
+			dns.peer.broadcast(Number160.createHash(args[1]));
 		}
 		if (args.length == 2) {
-			System.out.println("Name:" + args[1] + " Message:" + dns.get(args[1]));
+			Message message = dns.get(args[1]);
+			System.out.println("Name: " + args[1] + "\nMessage: " + message);
+			dns.store(args[1], message.getMessage());
 		}
+		System.out.println("Type anything to leave the network.");
+		new Scanner(System.in).nextLine();
+		dns.peer.shutdown();
 	}
 
 	private Message get(String name) throws ClassNotFoundException, IOException {
@@ -40,11 +54,12 @@ public class ExampleSimple {
 		if (futureDHT.isSuccess()) {
 			return ((Message) futureDHT.getData().getObject());
 		}
+		System.out.println("Unable to get key!");
 		return null;
 	}
 
-	private void store(String name) throws IOException {
-		peer.put(Number160.createHash(name)).setData(new Data(new Message("Hello World!"))).start().awaitUninterruptibly();
+	private void store(String name, String message) throws IOException {
+		peer.put(Number160.createHash(name)).setData(new Data(new Message(message))).start().awaitUninterruptibly();
 	}
 
 }
